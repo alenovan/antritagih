10d867816149d362d46fa2f39e451a24b4e2d402
alenovanwiradhika <ale@bdn.id>
2025-06-28 14:52:03 +0700

    faet : update

--- actions/app-actions.ts
+++ actions/app-actions.ts
@@ -1,7 +1,6 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { postMessage } from "@/app/[locale]/(protected)/app/chat/utils";
 
 export const postMessageAction = async (id: string, message: string) => {
   const response = await postMessage(id, message);
